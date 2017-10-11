0efeccefb0d9da0025bd72564e57eb432180f44d
TIMBIOZ-PC\Timbioz <volkovtim@gmail.com>
2017-10-11 08:28:11 +0300

    awef

---
+++ pages/user/index.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <div>
+      <h1 class="title">
+        User
+      </h1>
+      <p>osegmnfs</p>
+      
+</div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      message: 5,
+      name: 'Timbiozzzz'
+    }
+  }
+}
+</script>
---
+++ pages/user/one.vue
@@ -0,0 +1,21 @@
+<template>
+
+    
+      <h1 class="title">
+        One
+      </h1>
+      
+
+</template>
+
+<script>
+
+export default {
+  data: function () {
+    return {
+      message: 5,
+      name: 'Timbiozzzz'
+    }
+  }
+}
+</script>
